082677cbfa879b3e25987b0d4d40b9c0a8bb4bad
이정우 <acg6138@naver.com>
2023-05-03 16:02:38 +0900

    [FEAT] : Redis설정, CacheManager설정

---
+++ src/main/java/v1/amachon/global/config/redis/CacheKey.java
@@ -0,0 +1,10 @@
+package v1.amachon.global.config.redis;
+
+import lombok.Getter;
+
+@Getter
+public class CacheKey {
+
+    public static final String USER = "user";
+    public static final int DEFAULT_EXPIRE_SEC = 60;
+}
