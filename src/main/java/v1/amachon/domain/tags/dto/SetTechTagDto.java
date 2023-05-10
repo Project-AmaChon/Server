effedd624d621ffe36e55535cfb2c276f7a1641c
이정우 <acg6138@naver.com>
2023-05-10 17:17:25 +0900

    [FEAT]: 기술 태그, 지역 태그 변경 메서드 구현

--- src/main/java/v1/amachon/domain/tags/dto/SetTechTagDto.java
+++ src/main/java/v1/amachon/domain/tags/dto/SetTechTagDto.java
@@ -1,17 +1,14 @@
 package v1.amachon.domain.tags.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-@Builder
-public class SetTagsDto {
-    private String regionTagName;
+@AllArgsConstructor
+public class SetTechTagDto {
     private List<String> techTagName;
 }
--- src/main/java/v1/amachon/domain/tags/service/RegionTagService.java
+++ src/main/java/v1/amachon/domain/tags/service/RegionTagService.java
@@ -6,9 +6,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import v1.amachon.domain.base.BaseException;
 import v1.amachon.domain.base.BaseResponseStatus;
+import v1.amachon.domain.member.entity.Member;
+import v1.amachon.domain.member.repository.MemberRepository;
 import v1.amachon.domain.tags.dto.RegionTagDto;
+import v1.amachon.domain.tags.dto.SetRegionTagDto;
 import v1.amachon.domain.tags.entity.regiontag.RegionTag;
 import v1.amachon.domain.tags.repository.RegionTagRepository;
+import v1.amachon.global.config.security.util.SecurityUtils;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,6 +23,7 @@ import java.util.stream.Collectors;
 public class RegionTagService {
 
     private final RegionTagRepository regionTagRepository;
+    private final MemberRepository memberRepository;
 
     @Cacheable(value = "regionTags")
     public List<RegionTagDto> getAllRegionTags() {
@@ -33,5 +38,15 @@ public class RegionTagService {
                 () -> new BaseException(BaseResponseStatus.INVALID_TAG)
         );
         return new RegionTagDto(tag);
+    }
+
+    public void changeRegionTag(SetRegionTagDto setRegionTagDto) throws BaseException {
+        Member member = memberRepository.findByEmail(SecurityUtils.getLoggedUserEmail()).orElseThrow(
+                () -> new BaseException(BaseResponseStatus.UNAUTHORIZED)
+        );
+        RegionTag regionTag = regionTagRepository.findByName(setRegionTagDto.getRegionName()).orElseThrow(
+                () -> new BaseException(BaseResponseStatus.INVALID_TAG)
+        );
+        member.changeRegion(regionTag);
     }
 }
