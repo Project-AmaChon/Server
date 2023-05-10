bdc276a2ebf7a6dd3745bc14dc470ada868dc014
이정우 <acg6138@naver.com>
2023-05-10 14:40:37 +0900

    [FEAT] : 회원 가입 구현

--- src/main/java/v1/amachon/domain/member/controller/MemberController.java
+++ src/main/java/v1/amachon/domain/member/controller/MemberController.java
@@ -1,10 +1,22 @@
 package v1.amachon.domain.member.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import v1.amachon.domain.base.BaseResponse;
+import v1.amachon.domain.member.dto.join.JoinDto;
+import v1.amachon.domain.member.service.MemberService;
 
 @RestController
+@RequiredArgsConstructor
 public class MemberController {
 
+    private final MemberService memberService;
+
+    @PostMapping("/join")
+    public BaseResponse<String> join(@RequestBody JoinDto joinDto) {
+        memberService.join(joinDto);
+        return new BaseResponse<>("회원 가입 성공!");
+    }
 }
