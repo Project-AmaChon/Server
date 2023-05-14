fe028bcb833628a281d329c92c0c28be015b8d2d
이정우 <acg6138@naver.com>
2023-05-14 14:09:56 +0900

    [UPDATE] : indent 수정

--- src/main/java/v1/amachon/domain/project/dto/ProjectDetailDto.java
+++ src/main/java/v1/amachon/domain/project/dto/ProjectDetailDto.java
@@ -2,6 +2,7 @@ package v1.amachon.domain.project.dto;
 
 import java.time.LocalDate;
 import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,16 +14,16 @@ import v1.amachon.domain.member.entity.Member;
 @AllArgsConstructor
 @Builder
 public class ProjectDetailDto {
-  private Long id;
-  private String title;
-  private String description;
-  private LocalDate recruitDeadline;
-  private int recruitNumber;
-  private LocalDate developPeriod;
-  private Long leaderId;
-  private Long regionTagId;
-  private List<Long> techTagIds;
-  private List<String> imageUrls;
-  // 추가: 프로젝트 팀에 참가 중인 인원들의 목록
-  private List<Member> teamMembers;
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDate recruitDeadline;
+    private int recruitNumber;
+    private LocalDate developPeriod;
+    private Long leaderId;
+    private Long regionTagId;
+    private List<Long> techTagIds;
+    private List<String> imageUrls;
+    // 추가: 프로젝트 팀에 참가 중인 인원들의 목록
+    private List<Member> teamMembers;
 }
