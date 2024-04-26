d35df6a1f925ecb543ebd4cd74f84c645e23c35c
이정우 <acg6138@naver.com>
2024-04-26 15:21:28 +0900

    [UPDATE] : 메시지 상세 조회를 messageRoomId를 거치지 않도록 url 수정

--- src/main/java/v1/amachon/message/controller/MessageController.java
+++ src/main/java/v1/amachon/message/controller/MessageController.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import v1.amachon.common.config.security.AuthenticatedMemberEmail;
 import v1.amachon.message.service.dto.MessageDto;
 import v1.amachon.message.service.dto.SendMessageRequest;
 import v1.amachon.message.service.dto.MessageRoomDto;
@@ -20,9 +21,8 @@ public class MessageController {
 
     @ApiOperation(value = "메시지 보내기(유저 ID 이용)", notes = "프로필을 통해 메시지를 보낼 때")
     @PostMapping("/messages/{memberId}/send")
-    public ResponseEntity<Void> sendMessageByMemberId(@RequestHeader("Authorization")String accessToken,
-                                                        @PathVariable("memberId") Long memberId,
-                                                        @RequestBody SendMessageRequest sendMessageRequest) {
+    public ResponseEntity<Void> sendMessageByMemberId(@AuthenticatedMemberEmail String email, @PathVariable("memberId") Long memberId,
+                                                      @RequestBody SendMessageRequest sendMessageRequest) {
         messageService.sendMessageByMemberId(memberId, sendMessageRequest);
         return ResponseEntity.ok().build();
     }
@@ -45,19 +45,19 @@ public class MessageController {
     @ApiOperation(value = "메시지 방 조회", notes = "메시지 방의 메시지 리스트를 반환")
     @GetMapping("/messages/room/{roomId}")
     public ResponseEntity<List<MessageDto>> getMessageRooms(@RequestHeader("Authorization")String accessToken,
-                                                          @PathVariable("roomId") Long roomId) {
+                                                            @PathVariable("roomId") Long roomId) {
         return ResponseEntity.ok(messageService.getMessages(roomId));
     }
 
     @ApiOperation(value = "메시지 조회", notes = "메시지 상세 정보를 반환")
-    @GetMapping("/messages/room/{roomId}/{messageId}")
+    @GetMapping("/messages/{messageId}")
     public ResponseEntity<MessageDto> getMessageDetail(@RequestHeader("Authorization")String accessToken,
-                                                          @PathVariable("messageId") Long messageId) {
+                                                       @PathVariable("messageId") Long messageId) {
         return ResponseEntity.ok(messageService.getMessageDetail(messageId));
     }
 
     @ApiOperation(value = "메시지 삭제", notes = "특정 메시지를 삭제")
-    @PostMapping("/messages/room/{roomId}/{messageId}/delete")
+    @DeleteMapping("/messages/{messageId}")
     public ResponseEntity<Void> deleteMessage(@RequestHeader("Authorization")String accessToken,
                                                      @PathVariable("messageId") Long messageId) {
         messageService.deleteMessage(messageId);
