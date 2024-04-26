57aa32eee01c87ff70a87fef966b7fe8122244a2
이정우 <acg6138@naver.com>
2024-04-26 15:24:20 +0900

    [FEAT] : CustomAuthenticationEntryPoint 클래스 구현

---
+++ src/main/java/v1/amachon/common/config/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,38 @@
+package v1.amachon.common.config.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import v1.amachon.common.advice.ErrorResponse;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        String responseBody = objectMapper.writeValueAsString(new ErrorResponse("로그인이 필요합니다.", UNAUTHORIZED.value()));
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(responseBody);
+    }
+}
